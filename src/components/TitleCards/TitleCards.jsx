96f5c17ae744e3c0816d7162ba580e32336b8707
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-04 12:57:55 +0530

    some css added

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TitleCards.css'
+
+const TiltleCards = () => {
+  return (
+    <div className='titlecards'>TiltleCards</div>
+  )
+}
+
+export default TiltleCards
