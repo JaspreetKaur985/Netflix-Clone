e1aca40063df1e4025bd27fc4453b02000253ff1
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-05 19:28:20 +0530

    footerModified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from './pages/Home/Home'
-import Navbar from './components/Navbar/Navbar'
+import { Routes,Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
-    <div className='login'></div>
+    <div className='login'>
+      <img src={logo} className='login' alt="" />
+    </div>
   )
 }
 
