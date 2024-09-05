0748ff5c065a12a594975117f4ff4bd6c7b414a0
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-05 17:03:46 +0530

    css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TiltleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -17,11 +18,12 @@ const Home = () => {
                 <p>Discovering his ties to a secret ancient order , a young
                     man living in modern Istanbul embarks on a quest to save
                     the city from an immortal enemy.
-                    <div className="hero-btns">
-                      <button className='btn'><img src={play_icon} alt="" />Play</button>
-                      <button className='btn dark-btn'><img src={info_icon} alt="" />More info</button>
-                    </div>
                 </p>
+                <div className="hero-btns">
+                  <button className='btn'><img src={play_icon} alt="" />Play</button>
+                  <button className='btn dark-btn'><img src={info_icon} alt="" />More info</button>
+                </div>
+                <TiltleCards/>
             </div>
         </div>
     </div>
