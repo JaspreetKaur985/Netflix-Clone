767a6e29ac6023c2aa08e877cfe1301f92373ab8
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-12 15:14:16 +0530

    TitleCard name modified

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
-import TiltleCards from '../../components/TitleCards/TitleCards'
+import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
@@ -23,14 +23,14 @@ const Home = () => {
                   <button className='btn'><img src={play_icon} alt="" />Play</button>
                   <button className='btn dark-btn'><img src={info_icon} alt="" />More info</button>
                 </div>
-                <TiltleCards/>
+                <TitleCards/>
             </div>
         </div>
         <div className='more-cards'>
-          <TiltleCards title={"Blockbuster Movies"} category={"top_rated"}/>
-          <TiltleCards title={"Only on Netflix"} category={"popular"}/> 
-          <TiltleCards title={"Upcoming"} category={"upcoming"}/>
-          <TiltleCards title={"Top picks for you"} category={"now_playing"}/>
+          <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+          <TitleCards title={"Only on Netflix"} category={"popular"}/> 
+          <TitleCards title={"Upcoming"} category={"upcoming"}/>
+          <TitleCards title={"Top picks for you"} category={"now_playing"}/>
 
         </div>
     </div>
