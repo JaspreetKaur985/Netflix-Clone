3d3fdf353f3233ac260bbc86a2f856ccbb9ba4f6
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-12 15:05:13 +0530

    login&signup UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,17 +5,19 @@ import logo from '../../assets/logo.png'
 const Login = () => {
 
   const [signState,setSignState] = useState("Sign In")
-  l
+  
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your name' />
+          {signState==="Sign Up"?
+            <input type="text" placeholder='Your name' />
+          : <></>}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
-          <button>Sign In</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -25,8 +27,9 @@ const Login = () => {
           </div>
         </form>
         <div className='form-switch'>
-          <p>New to Netflix? <span>Sign Up Now</span></p>
-          <p>Already have an Account? <span>Sign In Now</span></p>
+          {signState==="Sign In"?
+            <p>New to Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>
+            :<p>Already have an Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>}
         </div>
       </div>
     </div>
