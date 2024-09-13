329906ba69c493300ed2d7878411d3fa4b5a02b5
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-13 12:24:48 +0530

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBruF38_BEbsYL1nS5oMg0WOfmRn9mcywU",
+  authDomain: "netflixclone-e2909.firebaseapp.com",
+  projectId: "netflixclone-e2909",
+  storageBucket: "netflixclone-e2909.appspot.com",
+  messagingSenderId: "536167101186",
+  appId: "1:536167101186:web:4ce80d76e1c2c98678881b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const signup = async(name,email,password)=>{
+    try{
+        const res = await createUserWithEmailAndPassword(auth,email,password)
+        const user = res.user
+        await addDoc(collection(db,"user"),{
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    }catch(error){
+        console.log(error)
+        alert(error)
+    }
+}
+
+const login = async(email,password)=>{
+    try{
+        await signInWithEmailAndPassword(auth,email,password)
+    }catch(error){
+        console.log(error)
+        alert(error)
+    }
+}
+
+const logout = ()=>{
+    signOut(auth)
+}
+
+export {auth,db,login,signup,logout};
